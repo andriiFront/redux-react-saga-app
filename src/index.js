b10e10c5796549c814fa682e1eda7c71f2c63b13
Andrii <badekha@gmail.com>
2022-11-19 20:52:47 +0200

    added middleware to filter bad words

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { rootReducer } from './redux/rootReducer';
+import { forbiddenWordsMiddleware } from './redux/middleware'
 
 const store = createStore(rootReducer, compose(
-  applyMiddleware(thunk),
+  applyMiddleware(thunk, forbiddenWordsMiddleware),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
