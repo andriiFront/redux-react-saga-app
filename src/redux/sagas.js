6235308655597816a04daf998e3099f0e18da313
Andrii <badekha@gmail.com>
2022-11-20 11:28:59 +0200

    added error handler in saga

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,17 +1,23 @@
 import { REQUEST_POSTS, FETCH_POSTS } from "./types";
 import { takeEvery, put, call } from 'redux-saga/effects'
-import { hideLoader, showLoader } from './actions'
+import { hideLoader, showLoader, showAlert } from './actions'
 
 export function* sagaWatcher() {
   yield takeEvery(REQUEST_POSTS, sagaWorker)
 }
 
 function* sagaWorker() {
-  yield put(showLoader())
-  const payload = yield call(fetchPosts)
-  yield put({ type: FETCH_POSTS, payload })
-  yield put(hideLoader())
-}
+  try {
+    yield put(showLoader())
+    const payload = yield call(fetchPosts)
+    yield put({ type: FETCH_POSTS, payload })
+    yield put(hideLoader())
+  } catch(e) {
+    const errMesssage = `${e.name}: ${e.message}`
+    yield put(showAlert(errMesssage))
+    yield put(hideLoader())
+  }
+}  
 
 async function fetchPosts() {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
